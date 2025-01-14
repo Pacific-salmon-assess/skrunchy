9f26cf3dcdffbaf520a3b44e816a39e0a1120128
Warkentin <Luke.Warkentin@dfo-mpo.gc.ca>
2025-01-14 15:40:53 -0800

    Update get_p example

--- R/get-p.R
+++ R/get-p.R
@@ -11,41 +11,7 @@
 #' @return A list with two elements. First element: numeric, an array of wild spawner proportions with values between 0 and 1, with three dimensions: population (i), year (y), and age (a).
 #'          Second element: data frame version of matrix, for plotting and tables.
 #' @examples
-#' library(abind)
-#' d <- make_P_G(start_year = 2000, end_year = 2001)
-#' res <- get_P_tilde(P = d$P, sigma_P = d$sigma_P, G = d$G)
-#' k <- data.frame( year = c(2000, 2001),
-#'                 kitsumkalum_escapement = c(5000, 6000),
-#'                 sd = c(500, 400))
-#' X <- get_X(P_tilde = res$P_tilde, sigma_P_tilde = res$sigma_P_tilde, K= k$kitsumkalum_escapement,
-#'           sigma_K = k$sd, y = k$year)
-#' Tau_U <- sample(50:100, size = length(k$year), replace=TRUE)
-#' E <- get_E(K = k$kitsumkalum_escapement, X = X$X, Tau_U = Tau_U,
-#'    known_population = "Kitsumkalum",
-#'     aggregate_population = "Skeena",
-#'     lower_populations = c("Lower Skeena", "Zymoetz-Fiddler"),
-#'     upper_populations = c("Upper Skeena", "Middle Skeena", "Large Lakes"))
-#'   populations <- c("Kitsumkalum", "Lower Skeena", "Zymoetz-Fiddler", "Upper Skeena", "Middle Skeena", "Large Lakes", "Skeena")
-#'   n_populations <- length(populations)
-#'   years <- 2000:2001
-#'   n_years <- length(years)
-#'   ages <- c(4,5,6,7)
-#'   p_ages <- c(30,40,40,1)
-#'   n_ages <- length(ages)
-#'   # Make up some age data
-#'   d <- sapply(p_ages, FUN = function(x){ rpois( n = n_populations*n_years, lambda= x) })
-#'   n <- array( d,  dim = c(n_populations, n_years, n_ages), dimnames = list(i = populations, y = years, a = ages))
-#'   omega <- get_omega(n)
-#'   K_star <- array(runif(n = n_years * n_ages, 5000, 20000), dim = c(n_years, n_ages), dimnames= list(y = years, a = ages))
-#'   E_star <- get_E_star(E = E$E, omega = omega$omega, K_star = K_star)
-#'   B_star <- array(sample(1:50, size= n_years*n_ages), dim = c(n_years, n_ages), dimnames = list( y = years, a = ages))
-#'   B_star[,4] <- 0 # make age 7 brood = 0 so you don't get negative fish in S_star
-#'   S_star <- get_S_star(E_star = E_star$E_star, B_star = B_star)
-#'   H_star <- array(sample(10:40, size=n_years*n_ages), dim = c(n_years, n_ages), dimnames = list( y = years, a = ages))
-#'   H_star[,4] <- 0 # make age 7 hatchery = 0 so you don't get negative fish in W_star
-#'   W_star <- get_W_star(S_star = S_star$S_star, H_star = H_star,
-#'     aggregate_population = "Skeena", hatchery_population = "Kitsumkalum")
-#'  p <- get_p(W_star = W_star$W_star, E_star = E_star$E_star)
+#'  p <- get_p(W_star = ex_W_star, E_star = ex_E_star)
 #'
 #' @export
 get_p <- function( W_star, E_star, hatchery_population = "Kitsumkalum",
