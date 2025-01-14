a0b9203369bd80151c1f31b9e2260370e39f9e7e
Warkentin <Luke.Warkentin@dfo-mpo.gc.ca>
2025-01-14 13:58:36 -0800

    Update get_E example

--- data-raw/make-example-data.R
+++ data-raw/make-example-data.R
@@ -18,16 +18,17 @@ ex_k <- read.csv(here("data-raw/kitsumkalum-escapement.csv"))
 ex_X <- get_X(P_tilde = ex_P_tilde, sigma_P_tilde = ex_sigma_P_tilde , K= ex_K,
            sigma_K = ex_sigma_K,
            y = k$year)
+ex_Tau_U <- sample(50:100, size = length(ex_k$year), replace=TRUE)
 
 
-
+# Save example data sets to data/ as .rda files
 usethis::use_data(ex_P, overwrite = TRUE)
 usethis::use_data(ex_sigma_P, overwrite = TRUE)
 usethis::use_data(ex_G, overwrite = TRUE)
 usethis::use_data(ex_P_tilde, overwrite = TRUE)
 usethis::use_data(ex_sigma_P_tilde, overwrite = TRUE)
-
 usethis::use_data(ex_k, overwrite = TRUE)
-
 usethis::use_data(ex_X, overwrite = TRUE)
+usethis::use_data(ex_Tau_U, overwrite = TRUE)
+
 #usethis::use_data(P_tilde, overwrite = TRUE)
