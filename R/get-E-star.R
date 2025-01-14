95f57d56ec3ca4aa599767eebcb51048d0e48b4d
Warkentin <Luke.Warkentin@dfo-mpo.gc.ca>
2025-01-14 14:26:35 -0800

    Update get_E_star example

--- R/get-E-star.R
+++ R/get-E-star.R
@@ -17,33 +17,7 @@
 #'          Second element: data frame version of first element, for plotting and tables.
 #'
 #' @examples
-#' library(abind)
-#' d <- make_P_G(start_year = 2000, end_year = 2001)
-#' res <- get_P_tilde(P = d$P, sigma_P = d$sigma_P, G = d$G)
-#' k <- data.frame( year = c(2000, 2001),
-#'                 kitsumkalum_escapement = c(5000, 6000),
-#'                 sd = c(500, 400))
-#' X <- get_X(P_tilde = res$P_tilde, sigma_P_tilde = res$sigma_P_tilde, K= k$kitsumkalum_escapement,
-#'           sigma_K = k$sd, y = k$year)
-#' Tau_U <- sample(50:100, size = length(k$year), replace=TRUE)
-#' E <- get_E(K = k$kitsumkalum_escapement, X = X$X, Tau_U = Tau_U,
-#'    known_population = "Kitsumkalum",
-#'     aggregate_population = "Skeena",
-#'     lower_populations = c("Lower Skeena", "Zymoetz-Fiddler"),
-#'     upper_populations = c("Upper Skeena", "Middle Skeena", "Large Lakes"))
-#'   populations <- c("Kitsumkalum", "Lower Skeena", "Zymoetz-Fiddler", "Upper Skeena", "Middle Skeena", "Large Lakes", "Skeena")
-#'   n_populations <- length(populations)
-#'   years <- 2000:2001
-#'   n_years <- length(years)
-#'   ages <- c(4,5,6)
-#'   p_ages <- c(30,40,40)
-#'   n_ages <- length(ages)
-#'   # Make up some age data
-#'   d <- sapply(p_ages, FUN = function(x){ rpois( n = n_populations*n_years, lambda= x) })
-#'   n <- array( d,  dim = c(n_populations, n_years, n_ages), dimnames = list(i = populations, y = years, a = ages))
-#'   omega <- get_omega(n)
-#'   K_star <- array(runif(n = n_years * n_ages, 5000, 20000), dim = c(n_years, n_ages), dimnames= list(y = years, a = ages))
-#'   E_star <- get_E_star(E = E$E, omega = omega$omega, K_star = K_star)
+#'   E_star <- get_E_star(E = ex_E, omega = ex_omega, K_star = ex_K_star)
 #'
 #' @export
 get_E_star <- function(E, omega,
--- data-raw/make-example-data.R
+++ data-raw/make-example-data.R
@@ -1,8 +1,12 @@
 ## code to prepare  dataset goes here
 
+# Exampled data is mostly made up, so don't use this for any analysis.
+# Toy data for package testing and examples.
+# Example data goes from return years 1984-2020.
+
 library(abind)
 
-d <- make_P_G(catch_range = c(0,100), n_weeks = 12, n_years = NA, start_year = 1984, end_year = 2023,
+d <- make_P_G(catch_range = c(0,100), n_weeks = 12, n_years = NA, start_year = 1984, end_year = 2020,
               population_names = c("Kitsumkalum", "Lower Skeena", "Middle Skeena", "Zymoetz-Fiddler", "Large Lakes", "Upper Skeena"),
               seed = TRUE)
 ex_P <- d$P
@@ -15,16 +19,21 @@ ex_sigma_P_tilde <- res$sigma_P_tilde
 
 ex_k <- read.csv(here("data-raw/kitsumkalum-escapement.csv"))
 
-ex_X <- get_X(P_tilde = ex_P_tilde, sigma_P_tilde = ex_sigma_P_tilde , K= ex_k$kitsumkalum_escapement,
+X <- get_X(P_tilde = ex_P_tilde, sigma_P_tilde = ex_sigma_P_tilde , K= ex_k$kitsumkalum_escapement,
            sigma_K = ex_k$sd,
            y = ex_k$year)
+ex_X <- X$X
 ex_Tau_U <- sample(50:100, size = length(ex_k$year), replace=TRUE)
 
-dimnames(ex_X$X)$i
-
-populations <- dimnames(ex_X$X)$i
+E <- get_E(K = ex_k$kitsumkalum_escapement, X = ex_X, Tau_U = ex_Tau_U,
+   known_population = "Kitsumkalum",
+    aggregate_population = "Skeena",
+    lower_populations = c("Lower Skeena", "Zymoetz-Fiddler"),
+    upper_populations = c("Upper Skeena", "Middle Skeena", "Large Lakes"))
+ex_E <- E$E
+populations <- dimnames(ex_X)$i
 n_populations <- length(populations)
-years <- dimnames(ex_X$X)$y
+years <- dimnames(ex_X)$y
 n_years <- length(years)
 ages <- c(4,5,6,7)
 p_ages <- c(30,40,40,1)
@@ -32,7 +41,18 @@ n_ages <- length(ages)
 # Make up some age data
 ad <- sapply(p_ages, FUN = function(x){ rpois( n = n_populations*n_years, lambda= x) })
 ex_n <- array( ad,  dim = c(n_populations, n_years, n_ages), dimnames = list(i = populations, y = years, a = ages))
+omega <- get_omega(ex_n)
+ex_omega <- omega$omega
 
+
+# read in Kitsumkalum age-specific escapement
+ksd <- read.csv(here("data-raw", "K_star.csv"))
+ksdl <- ksd %>% tidyr::pivot_longer(c("X4", "X5", "X6", "X7"), names_to = "a", values_to = "K_star")
+ksdl$a <- sub("X", "", ksdl$a)
+dim_order_K_star <- c("y","a")
+dim_position_K_star <- sapply(dim_order_K_star, function(x) grep( paste0("^", x, "$"), names(ksdl)))
+K_star <- tapply(ksdl$K_star, ksdl[ , dim_position_K_star], FUN=print, default = 0)
+ex_K_star <- K_star
 
 # Save example data sets to data/ as .rda files
 usethis::use_data(ex_P, overwrite = TRUE)
@@ -43,6 +63,9 @@ usethis::use_data(ex_sigma_P_tilde, overwrite = TRUE)
 usethis::use_data(ex_k, overwrite = TRUE)
 usethis::use_data(ex_X, overwrite = TRUE)
 usethis::use_data(ex_Tau_U, overwrite = TRUE)
+usethis::use_data(ex_E, overwrite = TRUE)
 usethis::use_data(ex_n, overwrite = TRUE)
+usethis::use_data(ex_omega, overwrite = TRUE)
+usethis::use_data(ex_K_star, overwrite = TRUE)
 
 #usethis::use_data(P_tilde, overwrite = TRUE)
