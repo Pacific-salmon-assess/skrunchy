aa4b8f2a154f675291eb01fbee9d4116f14152e7
Warkentin <Luke.Warkentin@dfo-mpo.gc.ca>
2025-01-22 10:48:02 -0800

    Update df_to_array

--- R/df_to_array.R
+++ R/df_to_array.R
@@ -7,7 +7,8 @@
 #' @param value Name of the dataframe variable to use for the array value.
 #' @param dimnames_order Variable names to use for dimensions of the new array (in the order you want them to be in the array)
 #' @param FUN Optional, function to use if there are more than 1 value for each combination of dimensions. Default is print().
-#' @param na.rm Optional, if FUN is arithmetic (e.g., sum), should NAs be removed? Default is TRUE.
+#'           Note that if FUN is arithmetic and you want to ignore NA values, it must be
+#'           specified like FUN = function(x) {sum(x, na.rm=TRUE)}
 #' @param default Value to use if there are missing values for combinations of dimension. Default is 0.
 #'
 #' @return An array with values equal to the dataframe value column, dimensions and dimension names as supplied.
@@ -19,10 +20,23 @@
 #' p <- runif(length(i), 0,1)
 #' d <- data.frame(i, y, w, p)
 #' arr <- df_to_array( df = d, value = "p", dimnames_order = c("i", "y", "w"))
+#'
+#' # Handling NA values, need to specify na.rm=TRUE within function statement.
+#' i <- c(rep("A", 4), rep("B", 9), rep("C", 16), rep("C", 16) )
+#' y <- c(rep(2000:2001, each = 2), rep(2000:2002, each = 3), rep(2000:2003, each=4), rep(2000:2003, each=4))
+#' w <- c(rep(1:2, times = 2), rep(1:3, times = 3), rep(1:4, times = 4),  rep(1:4, times = 4))
+#' p <- c(runif(length(i)-8, 0,1), rep(NA, 8))
+#' d <- data.frame(i, y, w, p)
+#' arr <- df_to_array( df = d, value = "p", FUN = sum, dimnames_order = c("i", "y", "w"))
+#' arr1 <- df_to_array( df = d, value = "p",
+#'       FUN = function(x) {sum(x, na.rm=TRUE)}, dimnames_order = c("i", "y", "w"))
+
+#' # Breaks if there are multiple values for any combination of dimensions.
+#' arr <- df_to_array( df = d, value = "p", FUN = print, dimnames_order = c("i", "y", "w"))
 
 #' @export
-df_to_array <- function( df, value, dimnames_order, FUN = print, na.rm=TRUE, default = 0) {
+df_to_array <- function( df, value, dimnames_order, FUN = print, default = 0) {
   dim_position <- sapply(dimnames_order, function(x) grep( paste0("^", x, "$"), names(df)) )
-  array <- tapply( df[ ,value], df[ ,dim_position], FUN = FUN, na.rm = na.rm, default = default)
+  array <- tapply( df[ ,value], df[ ,dim_position], FUN = FUN, default = default)
   return(array)
 }
