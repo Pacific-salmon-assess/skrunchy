d98bde8b688d8fc42effe800ebc806ecb691a66a
Warkentin <Luke.Warkentin@dfo-mpo.gc.ca>
2025-11-12 15:34:29 -0800

    Add functions for getting stage of return (jack/adult) and to get Alaska style fishing statistical week from a date

---
+++ R/get-stage.R
@@ -0,0 +1,47 @@
+#' Get stage (adult or jack)
+#'
+#' Uses Skeena River Tyee Test Fishery biodata to assign stage of return (adult or jack) to Chinook, based on
+#' CWT age, scale age, POH, and comments.
+#'
+#' In skrunchy, the main purpose of this function is to filter the complete biodata to estimate
+#' weekly catch of adult Chinook, which are used to do the weekly expansions of GSI proportions.
+#' Default POH cutoff length is 450 mm. Note that this is what is used at Kitsumkalum.
+#'
+#' @param scale_age Character, vector of Gilbert-Rich scale ages (including partial ages) from Sclerochronology Lab.
+#' @param cwt_age Numeric, vector of ages from Coded Wire Tags. Gilbert-Rich total age (return year minus brood year).
+#' @param POH Numeric, post-orbital hypural length, in mm
+#' @param comments Character, vector of comments
+#' @param jack_ages Character, vector of which scale_age values to include as jacks. Defaults to c("32", "31", "1M")
+#' @param jack_cutoff_length_POH Numeric, single value of what POH length to use as a cutoff for jacks. Defaults to 450 mm.
+#'
+#' @returns Character, "jack" or "adult"
+#'
+#' @examples
+#' scale_age <- c("31", "32", "42", "52", "62", "1M", "2M", "3M", "4M", NA, NA)
+#' cwt_age <- c(3, 3, 4, 5, 6, 3, 4, 5, 6, NA, NA)
+#' POH <- c(440, 420, 500, 600, 700, 380, 500, 600, 700, NA, NA)
+#' comments <- c("Jack", "Jack", NA, NA, NA, NA, NA, NA, NA, "jack, bitten", "Jack")
+#' d <- data.frame(scale_age, cwt_age, POH, comments)
+#' d$revised_stage <- get_stage( scale_age = scale_age, cwt_age = cwt_age, POH = POH, comments = comments)
+#'
+#'
+#' @export
+get_stage <- function(scale_age, cwt_age, POH, comments, jack_ages = c("32", "31", "1M"), jack_cutoff_length_POH = 450) {
+
+  # check lengths equal
+  if(!all( length(scale_age) == c(length(cwt_age), length(POH), length(comments)) ))  {
+    stop("Length of input variables not equal.") }
+  # New variable for revised stage
+  stage <- character( length = length(scale_age) )
+  # Main function
+  for(i in 1:length(scale_age)) {
+  stage[i] <- ifelse( isTRUE(cwt_age[i] <4), "jack", # If CWT age is less than 4, it is a jack
+                   ifelse(scale_age[i] %in% jack_ages , "jack",# If scale age is a jack age, it is a jack
+                          # If there is no scale age, CWT age, and length is less than jack cutoff, it is a jack
+                          ifelse( all( is.na(scale_age[i]) && is.na(cwt_age[i]), !is.na(POH[i]) && POH[i] <= jack_cutoff_length_POH ), "jack",
+                                  # If there is no length, scale age, or CWT age, and it is jack in comments, it is a jack
+                                  ifelse( all( is.na(POH[i]) && is.na(scale_age[i]), is.na(cwt_age[i]) && grepl("Jack|jack", comments[i])), "jack",
+                                          "adult"))))
+    }
+  return(stage)
+}
---
+++ R/get-stat-week.R
@@ -0,0 +1,23 @@
+#' Get Alaska style statistical week from a date
+#'
+#' This is used to group catch at Tyee into stat weeks, for weekly gill net catch
+#' and as a utility function. See https://mtalab.adfg.alaska.gov/CWT/reports/sbp_calendar.aspx
+#'
+#'
+#' @param date Date, yyyy-mm-dd format date
+#'
+#' @returns Integer, which stat week the date corresponds to.
+#'
+#' @examples
+#' d <- data.frame( "date" = seq.Date(ISOdate(2021,1,1), ISOdate(2025,12,31)))
+#' d$stat_week <- get_stat_week(d$date)
+#'
+#' @export
+get_stat_week <- function(date) {
+  # the "%U" format gives: The week number of the year (Sunday as the first day of the week)
+  # as a zero-padded decimal number (e.g. "00" to "53").
+  stat_week <- ifelse(
+    strftime( ISOdate( as.integer( strftime( date, format = "%Y" )), 1, 1), format = "%A" ) == "Sunday", # is the first day of the year a Sunday?
+    as.integer( strftime( date, format = "%U" )),  # If January 1 is a Sunday, then don't add 1 to %U format week because it starts at 1 if January 1 is a Sunday
+    as.integer( strftime( date, format = "%U" )) + 1 ) # If January 1 is not a Sunday, then weeks start at 0 and you have to add 1
+}
