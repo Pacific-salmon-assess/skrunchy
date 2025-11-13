a44b3c1cfcace8d3900f65a613a7aff175d802da
Warkentin <Luke.Warkentin@dfo-mpo.gc.ca>
2025-11-13 11:46:41 -0800

    Add adult age argument to get_stage function

--- R/get-stage.R
+++ R/get-stage.R
@@ -8,10 +8,13 @@
 #' Default POH cutoff length is 450 mm. Note that this is what is used at Kitsumkalum.
 #'
 #' @param scale_age Character, vector of Gilbert-Rich scale ages (including partial ages) from Sclerochronology Lab.
-#' @param cwt_age Numeric, vector of ages from Coded Wire Tags. Gilbert-Rich total age (return year minus brood year).
+#' @param cwt_age Numeric, vector of total ages from Coded Wire Tags (or a combination of scale ages and CWT ages). Gilbert-Rich total age (return year minus brood year).
 #' @param POH Numeric, post-orbital hypural length, in mm
 #' @param comments Character, vector of comments
 #' @param jack_ages Character, vector of which scale_age values to include as jacks. Defaults to c("32", "31", "1M")
+#' @param adult_ages Character, vector of which scale_age values to include as adults.
+#' Defaults to c("41", "42", "43", "51", "52", "53", "61", "62", "63", "71", "72", "73",
+#' "81", "82", "83", "2M", "3M", "4M", "5M" )
 #' @param jack_cutoff_length_POH Numeric, single value of what POH length to use as a cutoff for jacks. Defaults to 450 mm.
 #'
 #' @returns Character, "jack" or "adult"
@@ -26,7 +29,13 @@
 #'
 #'
 #' @export
-get_stage <- function(scale_age, cwt_age, POH, comments, jack_ages = c("32", "31", "1M"), jack_cutoff_length_POH = 450) {
+get_stage <- function(scale_age, cwt_age, POH, comments,
+                      jack_ages = c("32", "31", "1M"),
+                      adult_ages = c("41", "42", "43", "51", "52", "53",
+                                     "61", "62", "63", "71", "72", "73",
+                                     "81", "82", "83",
+                                     "2M", "3M", "4M", "5M" ),
+                      jack_cutoff_length_POH = 450) {
 
   # check lengths equal
   if(!all( length(scale_age) == c(length(cwt_age), length(POH), length(comments)) ))  {
@@ -37,13 +46,14 @@ get_stage <- function(scale_age, cwt_age, POH, comments, jack_ages = c("32", "31
   for(i in 1:length(scale_age)) {
   stage[i] <- ifelse( isTRUE(cwt_age[i] <4), "jack", # If CWT age is less than 4, it is a jack
                    ifelse(scale_age[i] %in% jack_ages , "jack",# If scale age is a jack age, it is a jack
+                          ifelse( scale_age[i] %in% adult_ages, "adult", # if scale age is an adult age, it is an adult.
                           # # If there is no scale age, CWT age, and length is less than jack cutoff, it is a jack
                           # ifelse( all( is.na(scale_age[i]) && is.na(cwt_age[i]), !is.na(POH[i]) && POH[i] <= jack_cutoff_length_POH ), "jack",
                           # If there is a POH length and it is less than the jack cutoff, it is a jack
                           ifelse( all( !is.na(POH[i]), POH[i] <= jack_cutoff_length_POH ), "jack",
                                   # If there is no length, scale age, or CWT age, and it is jack in comments, it is a jack
                                   ifelse( all( is.na(POH[i]) && is.na(scale_age[i]), is.na(cwt_age[i]) && grepl("Jack|jack", comments[i])), "jack",
-                                          "adult"))))
+                                          "adult")))))
     }
   return(stage)
 }
