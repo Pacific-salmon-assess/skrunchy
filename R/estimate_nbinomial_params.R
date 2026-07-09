2c37600240de7fcae922513702f9535640e8de4d
Mairin Deith <mairin.deith@gmail.com>
2026-07-08 17:31:58 -0700

    New function: use MLE to estimate rbinomial sampling params and begin implementation

---
+++ R/estimate_nbinomial_params.R
@@ -0,0 +1,20 @@
+#' Uses maximum likelihood estimation to find the most likely parameters to
+#' use to populate a negative binomial sampler.
+#' This was designed to accomodate the K parameter, but can be generalized
+#' to sample from negative binomial probability mass functions.
+#' The results of this function can be directly supplied to `rnbinomial()` when
+#' performing Monte carlo sampling.
+#'
+#' @param X Numeric, assumed to be comprised of all observed count data (e.g. K)
+#' used for estimating mu, size, and prob parameters for a negative binomial sampler.
+
+estimate_nbinomial_params <- function(X) {
+    if (mean(X) >= var(X)) stop("Mean of the dataset is greater than or equal to its variance; consider a Poisson distribution")
+    fit <- MASS::fitdistr(X, "Negative Binomial")
+    return(list(
+        mu = as.double(fit$estimate["mu"]),
+        size = as.double(fit$estimate["size"]),
+        prob = as.double(fit$estimate["mu"]) / (
+            as.double(fit$estimate["size"]) + as.double(fit$estimate["mu"]))
+    ))
+}
--- R/rru.R
+++ R/rru.R
@@ -20,44 +20,44 @@
 #'
 #' @examples
 #'
-#'  n_gsi_samples <- apply(ex_n, 2, sum)
-#'  n_age_samples <- apply(ex_n, c(1,2), sum)
-#'  n_age_samples_J <- apply(ex_n_with_jacks, c(1,2), sum)
-#'  results <- rru(
-#'    P = ex_P,
-#'    sigma_P = ex_sigma_P,
-#'    G = ex_G,
-#'    K = ex_k$kitsumkalum_escapement,
-#'    n_gsi_samples = n_gsi_samples,
-#'    n_age_samples = n_age_samples,
-#'    n_age_samples_J = n_age_samples_J,
-#'    sigma_K = ex_k$sd,
-#'    y_K = ex_k$year,
-#'    omega = ex_omega,
-#'    omega_J = ex_omega_J,
-#'    tyee = ex_Tau$tyee,
-#'    rec_catch_L = ex_Tau$rec_catch_L,
-#'    rec_release_L = ex_Tau$rec_release_L,
-#'    FN_catch_L = ex_Tau$FN_catch_L,
-#'    rec_catch_U = ex_Tau$rec_catch_U,
-#'    FN_catch_U = ex_Tau$FN_catch_U,
-#'    known_population = "Kitsumkalum",
-#'    aggregate_population = "Skeena",
-#'    lower_populations = c("Lower Skeena", "Zymoetz"),
-#'    upper_populations = c("Upper Skeena", "Middle Skeena", "Large Lakes"),
-#'    K_star = ex_K_star,
-#'    add_6_7 = TRUE,
-#'    B_star = ex_B_star,
-#'    H_star = ex_H_star,
-#'    tau_dot_M = ex_tau_dot_M,
-#'    phi_dot_M = ex_phi_dot_M,
-#'    r = ex_r,
-#'    phi_dot_E = ex_phi_dot_E,
-#'    Q = ex_Q,
-#'    name_key = variable_name_key,
-#'    save_outputs = FALSE,
-#'    iteration_number  = 1
-#'    )
+#' n_gsi_samples <- apply(ex_n, 2, sum)
+#' n_age_samples <- apply(ex_n, c(1, 2), sum)
+#' n_age_samples_J <- apply(ex_n_with_jacks, c(1, 2), sum)
+#' results <- rru(
+#'   P = ex_P,
+#'   sigma_P = ex_sigma_P,
+#'   G = ex_G,
+#'   K = ex_k$kitsumkalum_escapement,
+#'   n_gsi_samples = n_gsi_samples,
+#'   n_age_samples = n_age_samples,
+#'   n_age_samples_J = n_age_samples_J,
+#'   sigma_K = ex_k$sd,
+#'   y_K = ex_k$year,
+#'   omega = ex_omega,
+#'   omega_J = ex_omega_J,
+#'   tyee = ex_Tau$tyee,
+#'   rec_catch_L = ex_Tau$rec_catch_L,
+#'   rec_release_L = ex_Tau$rec_release_L,
+#'   FN_catch_L = ex_Tau$FN_catch_L,
+#'   rec_catch_U = ex_Tau$rec_catch_U,
+#'   FN_catch_U = ex_Tau$FN_catch_U,
+#'   known_population = "Kitsumkalum",
+#'   aggregate_population = "Skeena",
+#'   lower_populations = c("Lower Skeena", "Zymoetz"),
+#'   upper_populations = c("Upper Skeena", "Middle Skeena", "Large Lakes"),
+#'   K_star = ex_K_star,
+#'   add_6_7 = TRUE,
+#'   B_star = ex_B_star,
+#'   H_star = ex_H_star,
+#'   tau_dot_M = ex_tau_dot_M,
+#'   phi_dot_M = ex_phi_dot_M,
+#'   r = ex_r,
+#'   phi_dot_E = ex_phi_dot_E,
+#'   Q = ex_Q,
+#'   name_key = variable_name_key,
+#'   save_outputs = FALSE,
+#'   iteration_number = 1
+#' )
 #'
 rru <- function(
   P,
@@ -137,11 +137,22 @@ rru <- function(
     ) /
       n_age_samples_J[y]
   }
+
   # Note that for now, sigma_P_tilde and sigma_K are not used in the same way as
   # in the deterministic run reconstruction
 
   # Get a monte carlo sample of K
   K_sample <- rnorm(n = length(K), mean = K, sd = sigma_K)
+  # ! # Apply a negative binomial instead:
+  # ! # An alternative parametrization (often used in ecology) is by the
+  # ! #   _mean_ 'mu' (see above), and 'size', the _dispersion parameter_,
+  # ! #   where 'prob' = 'size/(size+mu)'.  The variance is 'mu + mu^2/size'
+  # ! #   in this parametrization.
+  # ! # Mairin has made a new function, estimate_nbinomial_params, for this
+
+  K_mu <- estimate_nbinomial_params(K)$mu
+  K_size <- estimate_nbinomial_params(K)$size
+  K_sample <- rnbinom(mu = K_mu, size = K_size)
 
   # Now do expansions to get returns to Terrace for each population, and the
   # Skeena aggregate.
@@ -303,7 +314,7 @@ rru <- function(
   # List of data with CU and return year and age
   list_df_iya <- list(
     # omega$df,
-    #"n" = n # n age observations, would need to make into df
+    # "n" = n # n age observations, would need to make into df
     E_star$df,
     B_star_df2,
     S_star$df,
@@ -358,7 +369,7 @@ rru <- function(
     all.x = TRUE
   )
 
-  #variable_name_key
+  # variable_name_key
 
   new_names <- names(dc)
   for (i in 1:length(names(dc))) {
@@ -370,7 +381,7 @@ rru <- function(
       names(dc)[i]
     )
   }
-  #new_names
+  # new_names
 
   dcn <- dc
   names(dcn) <- new_names
